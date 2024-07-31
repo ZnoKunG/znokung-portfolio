bfb4fd22635f9596fed99c56e4ad31728730a400
ZnoKunG <69045454+ZnoKunG@users.noreply.github.com>
2024-07-31 12:28:09 -0400

    change intro desc

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,7 @@ function Intro() {
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md: mb-3 font-bold'>Kittaphot</h1>
             <p className='text-base md:text-xl mb-3 font-medium'>Game Programmer & Full Stack Developer</p>
-            <p className='text0sm max-w-xl mb-2 font-bold'>Hi, I'm Kittaphot 3rd year Information and Communication Engineering Student at Chulalongkorn University.
-            I am passionate about games and love implementing logics in games. Moreover, I also enjoy creating websites and backend servers to process request from front-end in both websites and game-side.</p>
+            <p className='text0sm max-w-xl mb-2 font-bold'>Hi, I'm Kittaphot Seangprachathanarak proficient on web and mobile app development. I also have extensive experience on game development especially Unity using C#. I have an interest on low level programming including graphics using OpenGL and parallel programming using CUDA. </p>
         </div>
     )
 }
