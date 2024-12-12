1c0226b15cca70204b5b121b04dc1de5b278c829
Kittaphot Saengprachathanarak <znokung@Kittaphots-MacBook-Air.local>
2024-12-12 13:28:21 +0800

    change detail

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md: mb-3 font-bold'>Kittaphot Saengprachathanarak</h1>
             <p className='text-base md:text-xl mb-3 font-medium'>Game Programmer & Full Stack Developer</p>
-            <p className='text0sm max-w-xl mb-2 font-bold'>Hi, I'm Kittaphot Seangprachathanarak proficient on web and mobile app development including deployment with docker image on various cloud platform including AWS and Digital Ocean. I also have extensive experience on game development especially Unity using C#. I have an interest on low level programming including graphics using OpenGL and parallel programming using CUDA. </p>
+            <p className='text0sm max-w-xl mb-2 font-bold'>4th year Information and Communication Engineering student with extensive experience in web, mobile application, and game development. My experience from two internships in both business and academic perspectives makes me familiar with a whole software development process from planning, design, implementation, and deployment. My other interests are low-level programming such as graphics, HPC, GPU, compilers, and operating systems. </p>
         </div>
     )
 }
