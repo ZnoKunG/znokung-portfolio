48b36b68577602852a3824d0a06e20d0d79d9d1e
Kittaphot Saengprachathanarak <znokung@Kittaphots-MacBook-Air.local>
2024-12-12 13:27:04 +0800

    Change name

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md: mb-3 font-bold'>Kittaphot</h1>
+            <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md: mb-3 font-bold'>Kittaphot Saengprachathanarak</h1>
             <p className='text-base md:text-xl mb-3 font-medium'>Game Programmer & Full Stack Developer</p>
             <p className='text0sm max-w-xl mb-2 font-bold'>Hi, I'm Kittaphot Seangprachathanarak proficient on web and mobile app development including deployment with docker image on various cloud platform including AWS and Digital Ocean. I also have extensive experience on game development especially Unity using C#. I have an interest on low level programming including graphics using OpenGL and parallel programming using CUDA. </p>
         </div>
